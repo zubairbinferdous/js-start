c7b9e23b1e6afe83f9902f8db66cb3f5d2a22c50
zubairbinferdous <zubidev71@gmail.com>
2023-03-07 14:05:31 +0600

    ok

--- intro-js/first.js
+++ intro-js/first.js
@@ -4,3 +4,24 @@
 // Name = 'zubair khan';
 // console.log(Name);
 
+// math need for programming 
+
+/**
+ * 1.  number ==> 55 , 44.4 , -90  ..
+ * 2.  integer ==>  ood , even : 2 , 5 ..
+ * 3.  float  ==> 2.5 , 13.67 .. dscimal ...
+ * 4.  operation : addition, substraction , multiplication , division , remainder ..
+ * 5.  percent ==>
+ * 6.  average ==>
+ * 7.  comparison : > , < , == , != , >= , <= ...  
+ * 8.  oikki: 2 kilo apple and 5 kilo mango 
+ * 9.  45 , 24 , 44 , 120 ,  2: max , min , avarage , sum 
+ * 10. serics 
+ * :: extra ::
+ * 1. prime 
+ * 2. permuitation 
+ * 3. area 
+ * :: alwayes :: 
+ * jodi lage 
+ * 
+ */
