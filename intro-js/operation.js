bd2a9cfffff7321e85beafef394a5bb9d004a00c
zubairbinferdous <zubidev71@gmail.com>
2023-03-09 05:24:08 +0600

    ok done

--- intro-js/operation.js
+++ intro-js/operation.js
@@ -1,5 +1,32 @@
-console.log("operation");
+
 
 var eggPrice = 20;
 var onionPrice = 10;
+var result = eggPrice + onionPrice ;
+var result2 = eggPrice - onionPrice ;
+var result3 = eggPrice * onionPrice ;
+var result4 = eggPrice / onionPrice ;
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+
+var tomatoPrice = 40 ;
+tomatoPrice = 60 ;
+var price1 = tomatoPrice + 30;
+price1+=60; 
+price1-=80;
+price1*= 200;
+price1/= 400;
+console.log( 'new tomato' ,price1);
+var age = 30;
+age++;
+console.log(age);
+var love = 60;
+love--;
+console.log(love)
+
 
