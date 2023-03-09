7154ef5eeeb3298ad43047444ed45176e4c91ce3
zubairbinferdous <zubidev71@gmail.com>
2023-03-10 02:13:19 +0600

    ok

--- intro-js/array.js
+++ intro-js/array.js
@@ -1,9 +1,13 @@
-var friend = ['sadman' , 'emon' , 'ridoy' , 'mim'];
+var friend = [ 40 ,'sadman' , 'emon' , 'ridoy' , 'mim' , 60 , 90 , "habibai"];
 console.log(friend[0]);
+friend[0] = 160;
 console.log(friend.length);
 console.log(friend.indexOf("mim"));
+console.log(friend.indexOf(130));
+console.log(friend)
 
 for (let index = 0; index < friend.length; index++) {
     const name = friend[index];
     console.log(name);
 }
+
