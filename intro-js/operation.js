780e14488275ba7f259827ae72df89002cdb8549
zubairbinferdous <zubidev71@gmail.com>
2023-03-09 18:00:25 +0600

    ok

--- intro-js/operation.js
+++ intro-js/operation.js
@@ -27,6 +27,55 @@ age++;
 console.log(age);
 var love = 60;
 love--;
-console.log(love)
+console.log(love);
+
+var firstName = "zubair";
+var lastName = "khan";
+var Name = firstName + " " +lastName;
+console.log(Name);
+
+var value1 = '30';
+var value2 =  40.44;
+var same = value1 + value2;
+console.log( 'result1' ,same);
+value1 = parseInt(value1);
+value2 = parseFloat(value2);
+// cover string to number 
+var same = value1 + value2;
+console.log('result2' , same);
+var number1 = 0.1;
+var number2 = 0.2;
+var sum = number1 + number2;
+sum = parseFloat(sum);
+sum = sum.toFixed(3)
+console.log(sum);
+// console.log(typeof sum)
+
+let ageMe = 16;                           // number 
+let data1 = "John";                      // string
+let data2 = {first:"Jane", last:"Duo"};  // object
+let truth = false;                      // boolean
+let sheets = ["HTML","CSS","JS"];       // array
+let arro; typeof a;                        // undefined
+let arrr0 = null;                           // null
+
+console.log(typeof(ageMe));
+console.log(typeof(data1));
+console.log(typeof(data2));
+console.log(typeof(truth));
+console.log(typeof(sheets));
+console.log(typeof(arro));
+console.log(typeof(arrr0));
+
+
+var mango = 10;
+var person = 3;
+var remaineder = mango % person;
+console.log(remaineder);
+// vagfol / 
+// vagses %
+
+
+
 
 
