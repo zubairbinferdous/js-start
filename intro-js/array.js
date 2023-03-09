179f5d15cffa366d9d51b3b38ac511e4445e8dfa
zubairbinferdous <zubidev71@gmail.com>
2023-03-10 05:11:01 +0600

    array.js

--- intro-js/array.js
+++ intro-js/array.js
@@ -11,3 +11,24 @@ for (let index = 0; index < friend.length; index++) {
     console.log(name);
 }
 
+var number = [70 , 50 , 60 , 120 ];
+number.push(888);
+console.log(number);
+number.pop();
+number.pop();
+var element = number.pop();
+console.log(element);
+console.log(number);
+
+console.log(5 > 6)
+console.log(5 == 6)
+console.log(5 != 6)
+console.log(6 != 6)
+console.log(6 === 6)
+console.log( 7 <= 6);
+console.log( 7 >= 6);
+
+
+number > number2 && number4 > number && ball < bat ;
+number < kom || hello > low || you > Poll ;
+
