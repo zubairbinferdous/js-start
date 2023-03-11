f1c91b87b8730ae6cec669a3c0d70e1d3e2f81e5
zubairbinferdous <zubidev71@gmail.com>
2023-03-11 06:01:28 +0600

    ok done

---
+++ intro-js/loop.js
@@ -0,0 +1,25 @@
+var name = "zubair";
+var color = "Yellow";
+var age = 25;
+var schoolName = "bangladesh bank high school";
+var friend = ['zubi' , 'rohim' , 'mahir' , 'jame' , 'vabi'];
+friend[3] = "zubairKhan";
+var position = friend.indexOf('rohim');
+var life = false;
+console.log(friend.length);
+// push in array 
+// pop in array 
+
+if(friend.length > 4 ){
+    console.log('hello one' )
+}else if(friend[4] == "min"){
+    console.log('hello two')
+}else{
+    console.log('hello three')
+}
+
+let b = 10;
+for(let a = 0; a < b ; a++ ){
+    console.log('hello world' , a);
+}
+// console.log("hello world") 
