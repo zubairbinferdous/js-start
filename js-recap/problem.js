b77e42a66c8269ef01f4d3646b4770ec7a2fedf1
zubairbinferdous <zubidev71@gmail.com>
2023-03-23 17:04:26 +0600

    ok

--- js-recap/problem.js
+++ js-recap/problem.js
@@ -1,108 +1,144 @@
-let country = 'bangladesh';
-let age = 30 ;
-let indepandent = true;
-let student = {name: 'zubair', age: 40 , class: 'eight'};
-let friends = ['hello', 'mim' , 'juntu' , 'pakhi' , 'bala'];
-function add(){
-    number = a + b ;
-    return number;
-}
+// let country = 'bangladesh';
+// let age = 30 ;
+// let indepandent = true;
+// let student = {name: 'zubair', age: 40 , class: 'eight'};
+// let friends = ['hello', 'mim' , 'juntu' , 'pakhi' , 'bala'];
+// function add(){
+//     number = a + b ;
+//     return number;
+// }
 
-if(friends.indexOf('zubair') !== -1){
-    console.log('he is in array');
-}else{
-    console.log('he is not in array');
-}
+// if(friends.indexOf('zubair') !== -1){
+//     console.log('he is in array');
+// }else{
+//     console.log('he is not in array');
+// }
 
-// concat 
+// // concat 
 
-const newFriendAge = [12 ,33, 41,55 ];
-const newArray = friends.concat(newFriendAge);
+// const newFriendAge = [12 ,33, 41,55 ];
+// const newArray = friends.concat(newFriendAge);
 
-console.log(typeof country);
-console.log(typeof(country));
-console.log(typeof(age));
-console.log(typeof(indepandent));
-console.log(typeof(student));
-console.log(Array.isArray(friends));
-console.log(typeof add);
-console.log(friends.includes('mim'));
-console.log(newArray);
-
-
-// slice and splice
-
-const friendName = [33,34,12,23,34,22,15,17,80,45];
-var part = friendName.slice(3 , 7);
-var splice = friendName.splice( 2 , 3 , 99 , 555 , 777);
-console.log(part);
-console.log(splice);
-console.log(friendName);
+// console.log(typeof country);
+// console.log(typeof(country));
+// console.log(typeof(age));
+// console.log(typeof(indepandent));
+// console.log(typeof(student));
+// console.log(Array.isArray(friends));
+// console.log(typeof add);
+// console.log(friends.includes('mim'));
+// console.log(newArray);
 
 
-// remove the same value
+// // slice and splice
 
-const  biraniName = ['zubair' , 'mim' , 'zubair' , 'rana' ,'zubi' , 'mim' , 'pakhi', 'rana' ,'pakhi'];
-// const nname = biraniName.indexOf('ranae');
+// const friendName = [33,34,12,23,34,22,15,17,80,45];
+// var part = friendName.slice(3 , 7);
+// var splice = friendName.splice( 2 , 3 , 99 , 555 , 777);
+// console.log(part);
+// console.log(splice);
+// console.log(friendName);
 
-function removeName(data) {
-    let newlist = [];
-    for (let i = 0; i < data.length; i++) {
-        const element = data[i];
-        if (newlist.includes(element) === false) {
-            newlist.push(element);
+
+// // remove the same value
+
+// const  biraniName = ['zubair' , 'mim' , 'zubair' , 'rana' ,'zubi' , 'mim' , 'pakhi', 'rana' ,'pakhi'];
+// // const nname = biraniName.indexOf('ranae');
+
+// function removeName(data) {
+//     let newlist = [];
+//     for (let i = 0; i < data.length; i++) {
+//         const element = data[i];
+//         if (newlist.includes(element) === false) {
+//             newlist.push(element);
+//         }
+//     }
+//     return newlist;
+// }
+// let result1 = removeName(biraniName);
+// console.log(result1);
+// // console.log(nname)
+
+
+// // output 1-50 
+// // if /3 is call foo
+// // if /5 is call bar 
+// // if /3&5 is call foobar
+
+// function fooBar(data) {
+//     for (let i = 1; i <= data; i++) {
+//          var number = i;
+//          if ( number % 3 === 0 && number % 5 === 0) {
+//             console.log('FooBer' , i);
+//          }else if(number % 5 === 0){
+//             console.log('ber' , i);
+//          }else if(number % 3 === 0 ){
+//             console.log('foo' , i );
+//          }
+//          else{
+//              console.log(number);
+//          }
+
+//     }
+// }
+// var giveData = 50;
+// var result = fooBar(giveData);
+
+
+// // 5 kj chal, 7 potato , 10 litter milk , price koto  
+
+// function price( rice , potato , milk ) {
+//     const ricePrice = 150;
+//     const potaotoPrice = 50;
+//     const milkPrice = 100;
+
+//     const totalRicePrice = ricePrice * rice;
+//     const totalPotatoPrice = potaotoPrice * potato;
+//     const totalMilkPrice = milkPrice * milk;
+
+//     const pricetotal = totalMilkPrice + totalPotatoPrice +totalRicePrice;
+//     return pricetotal;
+
+// }
+
+// var unit = price(  5 , 7 , 5 );
+// var resultPrice = unit;
+// console.log( 'total price of this ' ,  resultPrice);
+
+
+
+const phone = [
+    {phone: 'samsum' , camara:33, storage: '32gb', price:90000, color:'red' },
+    {phone: 'nokia' , camara:33, storage: '32gb',  price:22000, color:'red' },
+    {phone: 'xaomi' , camara:33, storage: '32gb',  price:55000, color:'red' },
+    {phone: 'hello' , camara:33, storage: '32gb',  price:150000, color:'red' }
+];
+
+function chepPhone(phones){
+    let myPhonePrice = phones[0];
+    for(let i = 0; i < phones.length; i++) {
+        const phone = phones[i];
+        if( phone.price > myPhonePrice.price ){
+            myPhonePrice = phone;
         }
     }
-    return newlist;
+
+    return myPhonePrice;
 }
-let result1 = removeName(biraniName);
-console.log(result1);
-// console.log(nname)
+const myPhone = chepPhone(phone);
+console.log('this is the big phone' ,  myPhone);
 
 
-// output 1-50 
-// if /3 is call foo
-// if /5 is call bar 
-// if /3&5 is call foobar
-
-function fooBar(data) {
-    for (let i = 1; i <= data; i++) {
-         var number = i;
-         if ( number % 3 === 0 && number % 5 === 0) {
-            console.log('FooBer' , i);
-         }else if(number % 5 === 0){
-            console.log('ber' , i);
-         }else if(number % 3 === 0 ){
-            console.log('foo' , i );
-         }
-         else{
-             console.log(number);
-         }
-
+function cheop(phones) {
+    let cheapPhone = phones[0];
+    for(let i = 0; i < phones.length; i++ ){
+        let element = phones[i];
+        if (element.price < cheapPhone.price) {
+            cheapPhone = element;
+        }
     }
-}
-var giveData = 50;
-var result = fooBar(giveData);
-
-
-// 5 kj chal, 7 potato , 10 litter milk , price koto  
-
-function price( rice , potato , milk ) {
-    const ricePrice = 150;
-    const potaotoPrice = 50;
-    const milkPrice = 100;
-
-    const totalRicePrice = ricePrice * rice;
-    const totalPotatoPrice = potaotoPrice * potato;
-    const totalMilkPrice = milkPrice * milk;
-
-    const pricetotal = totalMilkPrice + totalPotatoPrice +totalRicePrice;
-    return pricetotal;
-
+    return cheapPhone;
 }
 
-var unit = price(  5 , 7 , 5 );
-var resultPrice = unit;
-console.log( 'total price of this ' ,  resultPrice);
-
-
+var result = cheop(phone);
+console.log('this is the small phone' ,result);
